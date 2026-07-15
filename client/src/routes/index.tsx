b98dd364f9aeb940570c25bd03c0dc6547730928
Mohamed Mourrane <mourranemohamed2020@gmail.com>
2026-07-15 16:27:20 +0100

    refactorisation du liste des destinations

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1676,7 +1676,7 @@ function Destinations({
           {t.destinations.empty}
         </p>
       ) : (
-        <div className="mt-12 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {destinationCards.map((d) => (
             <button
               key={d.name}
